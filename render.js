54884a52eb76c61431e80db67a12d59ac20b438a
Jesse Fulton <jessefulton+github@gmail.com>
2012-01-22 11:10:33 -0800

    working on baseline, font sizes, etc.

--- render.js
+++ render.js
@@ -9,6 +9,8 @@ page.onAlert = function(str) { console.log(str); }
 page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
     // jQuery is loaded, now manipulate the DOM
 	var newHtml = (page.evaluate(function () {
+		//.ocr_page .ocr_carea .ocr_par .ocr_line .ocr_word .ocr_xword
+	/*
 		$('.ocr_line[title~="bbox"], .ocr_line').each(function() {
 			var maxHeight = 0;
 			var baseline = 0;
@@ -20,7 +22,7 @@ page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
 				var brx = (matches[3]);
 				var bry = (matches[4]);
 				maxHeight = (maxHeight > (bry-tly)) ? maxHeight : (bry-tly);
-				baseline = Math.max(baseline, brx, bry);
+				baseline = Math.max(baseline, bry);
 			} catch(e) {
 				console.log(e);
 			}
@@ -36,9 +38,31 @@ page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
 			$(this).css("left", tlx+"px");
 			//$(this).css("bottom", baseline+"px");								
 		});
+*/
 
 
-		$('*[title~="bbox"]').each(function() {
+		$('.ocr_line[title~="bbox"]').each(function() {
+			var maxHeight = 0;
+			var baseline = 0;
+			try {
+				var t = ($(this).attr('title'));
+				var matches = t.match(/bbox (\d+) (\d+) (\d+) (\d+)/);//indexOf("bbox");
+				var tlx = (matches[1]);
+				var tly = (matches[2]);
+				var brx = (matches[3]);
+				var bry = (matches[4]);
+				maxHeight = (maxHeight > (bry-tly)) ? maxHeight : (bry-tly);
+				baseline = Math.max(baseline, bry);
+			} catch(e) {
+				console.log(e);
+			}
+			$(this).css("position", "absolute");
+			$(this).css("top", (maxHeight+baseline)+"px");
+			$(this).css("font-size", maxHeight+"px");
+		});
+
+
+		$('.ocr_word[title~="bbox"]').each(function() {
 			var t = ($(this).attr('title'));
 			var matches = t.match(/bbox (\d+) (\d+) (\d+) (\d+)/);//indexOf("bbox");
 			var tlx = (matches[1]);
@@ -46,14 +70,14 @@ page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
 			var brx = (matches[3]);
 			var bry = (matches[4]);
 			$(this).css("position", "absolute");
-			$(this).css("top", tly+"px");
+			//$(this).css("top", tly+"px");
 			$(this).css("left", tlx+"px");
 			$(this).css("width", (brx-tlx)+"px");
 			$(this).css("height", (bry-tly)+"px");
+			//$(this).css("border", "1px solid green");
 //			$(this).css("font-size", (bry-tly)+"px");		
 		});
 		return "<html>" + $("html").html() + "</html>";
-		//return document.documentElement.innerHTML;
 	}));
 
 	fs.write('output.html', newHtml, 'w');
