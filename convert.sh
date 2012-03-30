07ee937d704c397f6872a45bf100a9f155d337be
Jesse Fulton <jessefulton+github@gmail.com>
2012-03-30 11:04:02 -0700

    cleaning up hocr script, adding directories

--- convert.sh
+++ convert.sh
@@ -13,6 +13,8 @@ fi
 # absolute directory of script
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+HOCR_DIR="${DIR}/processed/hocr"
+CAPTCHAFY_DIR="${DIR}/processed/captchafy"
 
 #TODO: check to see if directory exists for this pdf
 #move into relative "processed" directory or something...
@@ -21,10 +23,24 @@ filename=$(basename $1)
 extension=${filename##*.}
 filename=${filename%.*}
 
-#convert -density 300 -compress None -monochrome -median 1 $1 $filename%03d.tiff
-#convert -density 300 $1 $filename%03d.png
+HOCR_DIR="${HOCR_DIR}/${filename}"
+CAPTCHAFY_DIR="${CAPTCHAFY_DIR}/${filename}"
 
-for filename in $(ls -a *.tiff)
+rm -rf $HOCR_DIR
+mkdir $HOCR_DIR
+
+convert -density 300 -compress None -monochrome -median 1 $1 "$HOCR_DIR/${filename}%03d.tiff"
+convert -density 300 $1 "$HOCR_DIR/${filename}%03d.png"
+
+pushd $HOCR_DIR
+
+for converted in $(ls -a *.tiff)
 do
-	tesseract $filename hocr_$filename -l eng ../hocr.config
+	newfilename=$(basename $converted)
+	newextension=${newfilename##*.}
+	newfilename=${newfilename%.*}
+	# tesseract $converted ./hocr/${newfilename} -l eng ../hocr.config
+	tesseract $converted ${newfilename} -l eng ${DIR}/hocr.config
 done;
+
+popd
